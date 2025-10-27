f1c049d79e2ada238eb94843ac03943d019c7cb3
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2025-10-27 15:35:55 +0530

    ...

---
+++ ilyafy/android/app/.cxx/Debug/2i4n145d/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/home/grishma/.gradle/caches/8.14.3/transforms/8c295ceb0cc9f36abb4e4e89a6f8655c/transformed/hermes-android-0.81.2-debug/prefab/modules/libhermes/libs/android.armeabi-v7a/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/home/grishma/.gradle/caches/8.14.3/transforms/8c295ceb0cc9f36abb4e4e89a6f8655c/transformed/hermes-android-0.81.2-debug/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
