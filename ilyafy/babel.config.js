dd78342d3f730deb8e7d7e75c62ad124fd73e16d
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2025-10-27 15:16:11 +0530

    Initialized Project

---
+++ ilyafy/babel.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  presets: ['module:@react-native/babel-preset', 'nativewind/babel'],
+  plugins: ['react-native-worklets/plugin']
+};
+
+// module.exports = {
+//   presets: [
+//     'module:metro-react-native-babel-preset',
+//     'nativewind/babel'
+//   ],
+//   plugins: [
+//     // Your other plugins, if any, go here.
+//     // For Reanimated v4, you should use `react-native-worklets/plugin`
+//     'react-native-worklets/plugin'
+//   ],
+// };
