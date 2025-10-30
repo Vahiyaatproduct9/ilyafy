57c14bc662f1175b87a0dff91da38120b95f5000
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2025-10-30 22:40:11 +0530

    added successful ws layover

--- ilyafy/android/app/.cxx/Debug/2i4n145d/prefab/x86/prefab/lib/i686-linux-android/cmake/react-native-worklets/react-native-workletsConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET react-native-worklets::worklets)
-add_library(react-native-worklets::worklets SHARED IMPORTED)
-set_target_properties(react-native-worklets::worklets PROPERTIES
-    IMPORTED_LOCATION "/home/grishma/Ideas/ilyafy/ilyafy/node_modules/react-native-worklets/android/build/intermediates/cxx/Debug/4a1n6x57/obj/x86/libworklets.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/grishma/Ideas/ilyafy/ilyafy/node_modules/react-native-worklets/android/build/prefab-headers/worklets"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
--- ilyafy/android/app/.cxx/Debug/2i4n145d/prefab/x86_64/prefab/lib/x86_64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "/home/grishma/.gradle/caches/8.14.3/transforms/8c295ceb0cc9f36abb4e4e89a6f8655c/transformed/hermes-android-0.81.2-debug/prefab/modules/libhermes/libs/android.x86_64/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "/home/grishma/.gradle/caches/8.14.3/transforms/8c295ceb0cc9f36abb4e4e89a6f8655c/transformed/hermes-android-0.81.2-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
