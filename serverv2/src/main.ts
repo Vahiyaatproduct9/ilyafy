9a0f597684e9182f6fbc1b5f47c4f0aef0e6e514
VahiyaatProduct9 <kishordebnath123123@gmail.com>
2026-01-04 20:47:10 +0530

    testing changes

--- serverv2/src/main.ts
+++ serverv2/src/main.ts
@@ -5,6 +5,9 @@ import { configDotenv } from 'dotenv';
 configDotenv();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: "*"
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true
   }))
